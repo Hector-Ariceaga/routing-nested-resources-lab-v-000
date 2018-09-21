f4836e9e6d8a41dd33f750fac16abfbf772c0320
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-21 23:07:14 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,6 +6,7 @@ class SongsController < ApplicationController
       else
         flash[:alert] = "Artist not found."
         @songs = Song.all
+        render 'songs/index'
       end
     else
       @songs = Song.all
