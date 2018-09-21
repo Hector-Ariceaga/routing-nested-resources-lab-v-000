f20c718a2eb121f016de77144b96522c67db7907
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-21 23:17:42 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,6 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      binding.pry
       if Artist.find_by(id: params[:artist_id])
         @songs = Artist.find(params[:artist_id]).songs
       else
@@ -15,7 +14,7 @@ class SongsController < ApplicationController
 
   def show
     if params[:artist_id]
-      @artist = Artist.find(params[:artist_id])
+      @artist = Artist.find_by(id: params[:artist_id])
       if @artist
         @song = @artist.songs.find(params[:song_id])
       else
