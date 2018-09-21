e328e252996464e478d9d8151c92ecefd9852336
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-21 23:19:23 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist
-        @song = @artist.songs.find(params[:song_id])
+        @song = @artist.songs.find(params[:id])
       else
         flash[:alert] = 'Song not found.'
       end
